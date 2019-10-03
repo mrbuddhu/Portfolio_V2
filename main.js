8ead8f28a3bbe00741d12d6a9c75fe297f05501e
Aryabhatta <mrbuddhu98@gmail.com>
2019-10-03 20:13:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -119,3 +119,48 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+
+//Flipping texts
+(function() {
+
+      var word = ['hello world', 'codepen', 'developer', 'thank you'];
+      var element = document.getElementsByTagName('p')[0];
+      var wordIndex = 1;
+
+      var resetAnimation = function() {
+        element.classList.remove('flip');
+      }
+
+      setInterval(function(){
+          switch (wordIndex) {
+            case 0:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 1;
+              setTimeout(resetAnimation, 1000);
+            break;
+
+            case 1:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 2;
+              setTimeout(resetAnimation, 1000);
+            break;
+
+            case 2:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 3;
+              setTimeout(resetAnimation, 1000);
+            break;
+
+            case 3:
+              element.classList.add('flip');
+              element.textContent = word[wordIndex];
+              wordIndex = 0;
+              setTimeout(resetAnimation, 1000);
+            break;
+          }
+      },2000)
+}());
