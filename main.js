553d81d6310ebe5d80ec4a28685723fe81f0a877
Aryabhatta <mrbuddhu98@gmail.com>
2019-11-27 10:26:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Flipping texts
 (function() {
 
-      var word = ['Mathematics           ', 'Computer Science        ', 'Development      ', 'Artificial Intelligence', 'Cybersecurity          ', 'Robotics         ', 'Cosmology         ', 'Fine Art               ', 'Education             '];
+      var word = ['1. Mathematics', '2. Computer Science', '3. Development', '4. AI', '5. Cybersecurity', '6. Robotics', '7. Cosmology', '8. Fine Art', '9. Education'];
       var element = document.getElementsByTagName('p')[0];
       var wordIndex = 1;
 
