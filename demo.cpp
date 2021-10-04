1cee40ff0f6e8f2390398cf0235d3c527c9073fc
Shabi Kaushal <i.am.msbuddhu@gmail.com>
2021-10-04 13:33:39 +0530

    Remove redundant file

--- demo.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hello world"<<endl;
-    cout<<"shabi kaushal";
-
-    return 0;
-}
--- demo1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int x;
-    cout << "Enter value for x ";
-    cin >> x; //Read into x
-    cout<< x;
-}
